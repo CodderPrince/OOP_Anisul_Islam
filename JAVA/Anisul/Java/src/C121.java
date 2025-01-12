944c799372eb62ff730e544ea5bbd471b11eb971
Md. An Nahian Prince <annahian44@gmail.com>
2025-01-13 02:33:33 +0600

    Create C121.java

---
+++ JAVA/Anisul/Java/src/C121.java
@@ -0,0 +1,14 @@
+public class C121 {
+
+  static class AddDemo {
+    // method
+    void add(int a, int b) {
+      System.out.println(a + b);
+    }
+  }
+
+  public static void main(String[] args) {
+    AddDemo ob = new AddDemo();
+    ob.add(1, 2  );
+  }
+}
