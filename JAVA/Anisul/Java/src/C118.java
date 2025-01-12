3e19b982f96d46f17e4569b450b0a451ed6eb8d1
Md. An Nahian Prince <annahian44@gmail.com>
2025-01-13 02:33:31 +0600

    Create C118.java

---
+++ JAVA/Anisul/Java/src/C118.java
@@ -0,0 +1,31 @@
+public class C118 {
+
+  static class Overload {
+    // no arguments
+    static void add() {
+      System.out.println("No arguments!\n");
+    }
+
+    // single arguments
+    /*static void add(int a) {
+      System.out.println("Single integer : " + a);
+    }*/
+
+    // singe arguments with different data types
+    static void add(double a) {
+      System.out.println("Single double : " + a);
+    }
+
+    // triple variables
+    static void add(int a, double b, int c) {
+      System.out.println("Add : " + (a + b + c));
+    }
+  }
+
+  public static void main(String[] args) {
+    Overload.add(); // no arguments
+    Overload.add(5); // int automatic convert to double
+    Overload.add(3.18); // double
+    Overload.add(5, 6.36652, 7); // triple
+  }
+}
